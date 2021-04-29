79717141696d5578ca77a732938d739d1c1b3959
NamihiroMori <82070687+NamihiroMori@users.noreply.github.com>
2021-04-29 13:59:05 +0900

    chapter1プロジェクト作成

---
+++ JavaStudy/chapter1/src/chapter1/Ex1.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        List<String> list = new ArrayList<String>();
+        System.out.println("Hello Java");
+
+    }
+
+}
